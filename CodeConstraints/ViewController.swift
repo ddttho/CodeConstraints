9cb83d1d7c9f9fe3cf187ddd197835cb6580176f
Tho Do <ddttho@FOSSIL.COM>
2017-04-19 00:32:23 +0700

    Code Constraint

---
+++ CodeConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeConstraints
+//
+//  Created by Tho Do on 4/19/17.
+//  Copyright © 2017 Tho Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
